a21c939e81b6ac625ea36b9b1e95869804994690
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-01-15 15:29:21 -0500

    Remove "?" from .admin method for User class.

--- db/schema.rb
+++ db/schema.rb
@@ -104,7 +104,7 @@ ActiveRecord::Schema.define(version: 2021_01_15_200453) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "password_digest"
-    t.boolean "admin?", default: false, null: false
+    t.boolean "admin", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
