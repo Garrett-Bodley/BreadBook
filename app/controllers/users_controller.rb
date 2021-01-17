7cc710e45aa995edacfe9f09c1ee86fe59c1cd87
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-01-17 18:26:45 -0500

    Build new, create, and show routes and views for User.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,28 @@
 class UsersController < ApplicationController
 
-  def login
-
+  def new
+    @user = User.new
   end
 
-  def logout
+  def create
+    @user = User.new(user_params)
+    byebug
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      render :new, alert: "Invalid input(s) recieved."
+    end
+  end
+
+  def show
+    @user = User.find(session[:user_id])
   end
   
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+
 end
