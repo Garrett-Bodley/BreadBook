4fa11b71c8e705e39b9527166b971ed86649552d
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-01-17 18:24:24 -0500

    Build basic Ingredient paths and views.

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,21 +1,51 @@
 class IngredientsController < ApplicationController
 
+  before_action :set_ingredient, only: [:edit, :update, :show]
+
   def new
+    @ingredient = Ingredient.new
   end
 
   def create
+    @ingredient = Ingredient.new(ingredients_params)
+    if @ingredient.save
+      redirect_to @ingredient
+    else
+      render :new, alert: "Invalid input(s) supplied"
+    end
   end
 
   def edit
+  end
+
+  def update
+    @ingredient.update(ingredients_params)
+    if @ingredient.save
+      redirect_to @ingredient, alert: "Your changes have been saved"
+    else
+      render :edit, alert: "Invalid input(s) supplied"
+    end
   end
 
   def show
   end
 
   def index
+    @ingredients = Ingredient.all
   end
 
   def destroy
+    byebug
+  end
+
+  private
+
+  def ingredients_params
+    params.require(:ingredient).permit(:name, :description)
+  end
+
+  def set_ingredient
+    @ingredient ||= Ingredient.find(params[:id]) if params[:id]
   end
 
 end
