766f5835c220f5af28e588e706904b9930e5beae
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-01-17 18:28:48 -0500

    Make Ingredient class validate :name.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -3,4 +3,5 @@ class Ingredient < ApplicationRecord
   has_many :recipes, through: :bakers_percentages
   has_many :bookmarks, as: :bookmarkable
   has_many :comments, as: :commentable
+  validates :name, presence: true
 end
