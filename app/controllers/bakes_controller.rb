ab47eed43ce868f38483828ece9455e2bbcdca12
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-01-17 22:03:45 -0500

    Add user_id to form partial, render partial in new & edit.

--- app/controllers/bakes_controller.rb
+++ app/controllers/bakes_controller.rb
@@ -1,15 +1,24 @@
 class BakesController < ApplicationController
 
   def new
+    @user = User.find(session[:user_id])
+    @bake = Bake.new
   end
 
   def create
+    @bake = Bake.new(bake_params)
+    if @bake.save
+      redirect_to @bake
+    else
+      render :new, alert: "Invalid input(s) provided."
+    end
   end
 
   def edit
   end
 
   def show
+    @bake = Bake.find(params[:id])
   end
 
   def index
@@ -18,4 +27,10 @@ class BakesController < ApplicationController
   def destroy
   end
   
+  private
+
+  def bake_params
+    params.require(:bake).permit(:date, :weight, :recipe_id, :user_id)
+  end
+
 end
