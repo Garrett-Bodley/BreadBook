ac8b34bcaa54ca6817444c5cad64d6d0412701b3
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-01-17 21:10:14 -0500

    Add aliased polymorphic relationships. Add has_many, through: relationships to Ingredient and Recipe.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,11 @@ class User < ApplicationRecord
   has_many :bakes
   has_many :comments
   has_many :bookmarks
+  has_many :posts
+  has_many :ingredients, through: :bookmarks, source: :bookmarkable, source_type: 'Ingredient'
+  has_many :recipes, through: :bookmarks, source: :bookmarkable, source_type: 'Recipe'
+  has_many :saved_comments, through: :bookmarks, source: :bookmarkable, source_type: 'Comment', class_name: "Comment"
+  has_many :saved_posts, through: :bookmarks, source: :bookmarkable, source_type: 'Post', class_name: "Post"
   has_many :likes
+
 end
