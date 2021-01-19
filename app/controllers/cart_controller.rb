ee3db630c0b27017b2eadeb9366785d457b76253
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-01-19 17:04:49 -0500

    Refactor controllers so cart logic is separate controller.

---
+++ app/controllers/cart_controller.rb
@@ -0,0 +1,19 @@
+class CartController < ApplicationController
+
+  def add
+    cart << cart_params unless cart.include?(cart_params)
+    redirect_to cart_path
+  end
+
+  def show
+    @recipe = current_user.recipes.build
+    cart.each{|id| @recipe.bakers_percentages.build(ingredient: Ingredient.find(id))}
+  end
+  
+  private
+
+  def cart_params
+    params.require(:ingredient).permit(:id)[:id].to_i
+  end
+
+end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,22 +9,13 @@ class RecipesController < ApplicationController
   #   end
   # end
 
-  def add
-    recipe_cart << recipe_cart_params.to_i unless recipe_cart.include?(recipe_cart_params)
-    redirect_to recipes_cart_path
-  end
-
-  def cart
-    @recipe = current_user.recipes.build
-    recipe_cart.each{|i| @recipe.bakers_percentages.build(ingredient: Ingredient.find(i))}
-  end
-
   def create
     @recipe = Recipe.new(recipe_params)
     if @recipe.save
+      session[:cart].clear
       redirect_to @recipe
     else
-      redirect_to recipes_cart_path, alert: "Something went wrong..."
+      redirect_to cart_path, alert: "Something went wrong..."
     end
   end
 
@@ -33,16 +24,17 @@ class RecipesController < ApplicationController
   end
 
   def index
+    @recipes = Recipe.all
+  end
+
+  def search
+
   end
 
   private
 
   def recipe_params
     params.require(:recipe).permit(:name, :description, bakers_percentages_attributes:[:ingredient_id, :percent])
-  end
-
-  def recipe_cart_params
-    params.require(:ingredient).permit(:id)[:id]
   end
   
 end
