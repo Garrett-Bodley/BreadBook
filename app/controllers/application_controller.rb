20d6f249b1f294dc116ad2a9908de13226e04362
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-01-19 17:05:05 -0500

    Make current_user and cart methods.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+
+  before_action :current_user
+
+  def cart
+    @cart ||= session[:cart] || []
+    session[:cart] ||= @cart
+  end
+
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  
 end
