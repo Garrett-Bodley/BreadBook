9779762f9712fda1ccec5e90b96313d658316470
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-01-19 17:37:41 -0500

    Make bakers_percentages dependent on parent object.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,8 +1,8 @@
 class Ingredient < ApplicationRecord
   belongs_to :user
-  has_many :bakers_percentages
+  has_many :bakers_percentages, dependent: :destroy
   has_many :recipes, through: :bakers_percentages
-  has_many :bookmarks, as: :bookmarkable
-  has_many :comments, as: :commentable
+  has_many :bookmarks, as: :bookmarkable, dependent: :destroy
+  has_many :comments, as: :commentable, dependent: :destroy
   validates :name, presence: true
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,9 +1,9 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :bakes
-  has_many :bakers_percentages
+  has_many :bakers_percentages, dependent: :destroy
   has_many :ingredients, through: :bakers_percentages
-  has_many :bookmarks, as: :bookmarkable
-  has_many :comments, as: :commentable
+  has_many :bookmarks, as: :bookmarkable, dependent: :destroy
+  has_many :comments, as: :commentable, dependent: :destroy
   accepts_nested_attributes_for :bakers_percentages
 end
