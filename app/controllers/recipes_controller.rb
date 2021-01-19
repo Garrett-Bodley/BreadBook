f169044985d50a86527fe2484f9d0ac0cf630fa5
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-01-19 14:35:26 -0500

    Build out controller for show, cart, add, & create actions.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,17 +1,35 @@
 class RecipesController < ApplicationController
 
-  def new
-    @recipe = Recipe.new
-    @ingredients = Ingredient.all
+  # def new
+  #   @recipe ||= Recipe.new
+  #   if params[:ingredient_count]
+  #     params[:ingredient_count].to_i.times{@recipe.bakers_percentages.build}
+  #   else
+  #     6.times{@recipe.bakers_percentages.build}
+  #   end
+  # end
+
+  def add
+    recipe_cart << recipe_cart_params.to_i unless recipe_cart.include?(recipe_cart_params)
+    redirect_to recipes_cart_path
+  end
+
+  def cart
+    @recipe = current_user.recipes.build
+    recipe_cart.each{|i| @recipe.bakers_percentages.build(ingredient: Ingredient.find(i))}
   end
 
   def create
-  end
-
-  def edit
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to @recipe
+    else
+      redirect_to recipes_cart_path, alert: "Something went wrong..."
+    end
   end
 
   def show
+    @recipe = Recipe.find(params[:id])
   end
 
   def index
@@ -20,7 +38,11 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:recipe).permit(:name, :description, :ingredients_id[], :bakers_percentages[])
+    params.require(:recipe).permit(:name, :description, bakers_percentages_attributes:[:ingredient_id, :percent])
+  end
+
+  def recipe_cart_params
+    params.require(:ingredient).permit(:id)[:id]
   end
   
 end
