e95277880baa48ac38fb57a806a40475d905e002
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-01-19 14:35:00 -0500

    Update routes for various resources.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  resources :steps
-  resources :comments
-  resources :posts
-  resources :users
-  resources :bakes
-  resources :bakers_percentages
+  # post '/recipes/new', to: 'recipes#new'
+  post '/recipes/add', to: 'recipes#add'
+  get '/recipes/cart', to: 'recipes#cart'
+  
+  resources :steps, :users, :bakes, :bakers_percentages, :recipes
+  resources :comments, :posts, :ingredients do
+    resources :bookmarks, only: [:create, :destroy]
+  end
+
   resources :recipes
-  resources :ingredients
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
