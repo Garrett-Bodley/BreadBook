8981ddecf6fc321774328839b4537ef54c54dfcf
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-01-19 13:51:23 -0500

    Make Recipe accept nested attributes for bakers_percentages.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,4 +4,5 @@ class Recipe < ApplicationRecord
   has_many :ingredients, through: :bakers_percentages
   has_many :bookmarks, as: :bookmarkable
   has_many :comments, as: :commentable
+  accepts_nested_attributes_for :bakers_percentages
 end
