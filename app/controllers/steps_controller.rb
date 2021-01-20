7850eadc568963ec9a56042d477c8700f53c6a22
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-01-20 15:05:11 -0500

    Build out new and create actions.

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -1,9 +1,16 @@
 class StepsController < ApplicationController
   
   def new
+    @current_step = current_bake.steps.build
   end
 
   def create
+    current_step = current_bake.steps.build
+    if current_step.save
+      redirect_to current_bake
+    else
+      redirect_to new_bake_step_path(current_bake), alert: "Something went wrong..."
+    end
   end
 
   def show
@@ -16,6 +23,16 @@ class StepsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def current_bake
+    @current_bake ||= Bake.find(params[:bake_id]) if params[:bake_id]
+  end
+
+  def current_step
+    @current_step ||= Step.find(params[:id]) if params[:id]
   end
     
 end
