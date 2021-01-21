0d65665b3106059af605f2d9a21fc5e90ba247b4
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-01-21 10:51:21 -0500

    Build out new and create actions.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,17 @@
 class PostsController < ApplicationController
+  before_action :set_post, except: [:new, :create, :index]
 
   def new
-    @post = Post.new
+    @post = @current_user.posts.build
   end
 
   def create
+    @post = @current_user.posts.build(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      redirect_to new_post_path, alert: "Something went wrong..."
+    end
   end
 
   def edit
@@ -14,6 +21,16 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id]) if params[:id]
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content, :bake_id)
   end
 
 end
