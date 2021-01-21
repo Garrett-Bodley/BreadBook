5a5e90919e57d9e797bebbfc18351265a55de5a6
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-01-21 15:26:15 -0500

    Build out #destroy action.

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,18 @@ class LikesController < ApplicationController
 
   def create
     @like = @current_user.likes.build(like_params)
-
     if @like.save
       redirect_to @like.likeable.commentable
     else
       redirect_to @like.likeable.commentable, alert: "Something went wrong..."
     end
+  end
+
+  def destroy
+    @like = Like.find(params[:id])
+    @parent = @like.likeable.commentable
+    @like.destroy
+    redirect_to @parent
   end
 
   private
