5c3f07b7702442ec6a9035ecaeae731546ed2e98
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-01-21 10:52:45 -0500

    Add #name instance method to Bake model.

--- app/models/bake.rb
+++ app/models/bake.rb
@@ -2,4 +2,9 @@ class Bake < ApplicationRecord
   belongs_to :recipe
   belongs_to :user
   has_many :steps
+
+  def name
+    "#{self.recipe.name} - #{self.date.strftime("%b %d, %Y")}"
+  end
+  
 end
