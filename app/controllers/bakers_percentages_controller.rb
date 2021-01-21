6b0a56f824cc6f762aad979dc592beae63b6c573
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-01-20 22:57:51 -0500

    Build new and create actions.

--- app/controllers/bakers_percentages_controller.rb
+++ app/controllers/bakers_percentages_controller.rb
@@ -1,2 +1,27 @@
 class BakersPercentagesController < ApplicationController
+
+  before_action :set_recipe
+
+  def new
+  end
+
+  def create
+    @recipe.bakers_percentages.build(percentage_params)
+    if @recipe.save
+      redirect_to @recipe, alert: "Ingredient added!"
+    else
+      redirect_to new_recipe_bakers_percentage_path(@recipe), alert: "Something went wrong..."
+    end
+  end
+
+  private
+
+  def percentage_params
+    params.require(:bakers_percentage).permit(:ingredient_id, :percent)
+  end
+
+  def set_recipe
+    @recipe = Recipe.find(params[:recipe_id])
+  end
+
 end
