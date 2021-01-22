3a20769281711f0ffc6a93ad5e7495bbdbdd3145
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-01-22 14:19:53 -0500

    Debug User validations/Omniauth action.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: :true, uniqueness: true
-  validates :password, presence: :true
+  validates :password, presence: :true, allow_nil: true
   has_secure_password
   has_many :bakes
   has_many :comments
