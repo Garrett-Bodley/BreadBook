35977fa932e2801de8bb5dd7827f0953af9c879a
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-01-23 16:48:00 -0500

    Add #logged_in? and #permission_check helpers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,5 +3,13 @@ module ApplicationHelper
   def current_user
     @current_user = User.find(session[:user_id]) if session[:user_id]
   end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def permission_check
+    redirect_to '/login', alert: "You must be logged in to view that page." unless logged_in?
+  end
   
 end
