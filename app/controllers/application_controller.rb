e0b8047370920eb7d5e98786a51cb1c662b3bf54
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-01-22 21:34:48 -0500

    Make #current_user a helper method included in controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,11 @@
 class ApplicationController < ActionController::Base
+  include ApplicationHelper
 
   before_action :current_user
 
   def cart
     @cart ||= session[:cart] || []
     session[:cart] ||= @cart
-  end
-
-  private
-
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   
 end
