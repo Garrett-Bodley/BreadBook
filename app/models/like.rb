f29081749d64e730f64f15f8884fe919432ef1b4
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-01-24 23:45:22 -0500

    Add class methods to determine parent object.

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,4 +2,17 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :likeable, polymorphic: true
   validates :user_id, uniqueness: {scope: [:likeable_id, :likeable_type]}
+
+  def liked_comment?
+    self.likeable.class.name == "Comment" ? true : false
+  end
+
+  def parent
+    if self.liked_comment?
+      self.likeable.commentable
+    else
+      self.likeable
+    end
+  end
+
 end
