369c501fb1b8a7882ad516fe58aec1f4fbb6fe1f
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-01-25 00:07:41 -0500

    Make recipes likeable. Add scope methods.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,10 +1,17 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  
   has_many :bakes
   has_many :bakers_percentages, dependent: :destroy
   has_many :ingredients, through: :bakers_percentages
   has_many :bookmarks, as: :bookmarkable, dependent: :destroy
   has_many :comments, as: :commentable, dependent: :destroy
+  has_many :likes, as: :likeable, dependent: :destroy
+
   accepts_nested_attributes_for :bakers_percentages
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
+
+  scope :order_by_likes, -> {joins(:likes).group(:likeable_id).order('COUNT(likes.likeable_id) DESC')}
+  scope :most_used, -> {joins(:bakes).group('bakes.recipe_id').order('COUNT(bakes.recipe_id) DESC').limit(30)}
+  scope :most_discussed, -> {joins(:comments).group('comments.commentable_id').order('COUNT(comments.commentable_id) DESC').limit(30)}
 end
