79ad5e5fc4bd644b8407293900be599a788f3924
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-01-24 20:46:08 -0500

    Add #edit and #update actions.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, except: :create
 
   def create
     @comment = @current_user.comments.build(comment_params)
@@ -9,14 +10,29 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @comment.update(comment_params)
+    if @comment.valid?
+      redirect_to @comment.commentable, alert: "Changes saved"
+    else
+      redirect_to @comment.commentable, alert: "Something went wrong..."
+    end
+  end
+
   def destroy
-    @comment = Comment.find(params[:id])
     @parent = @comment.commentable
     @comment.destroy
     redirect_to @parent
   end
 
   private
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
 
   def comment_params
     params.require(:comment).permit(:content, :commentable_id, :commentable_type)
