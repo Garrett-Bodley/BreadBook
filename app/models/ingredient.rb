59d66d8fa3c8464ad10747374ad8072ae5e50007
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-01-25 00:06:49 -0500

    Make ingredient likeable. Add scope methods.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -4,5 +4,11 @@ class Ingredient < ApplicationRecord
   has_many :recipes, through: :bakers_percentages
   has_many :bookmarks, as: :bookmarkable, dependent: :destroy
   has_many :comments, as: :commentable, dependent: :destroy
-  validates :name, presence: true
+  has_many :likes, as: :likeable, dependent: :destroy
+  
+  validates :name, presence: true, uniqueness: true
+
+  scope :order_by_likes, -> {joins(:likes).group(:likeable_id).order('COUNT(likes.likeable_id) DESC')}
+  scope :most_used, ->{joins(:bakers_percentages).group('bakers_percentages.ingredient_id').order('COUNT(bakers_percentages.ingredient_id) DESC').limit(30)}
+  scope :most_discussed, -> {joins(:comments).group('comments.commentable_id').order('COUNT(comments.commentable_id) DESC').limit(30)} 
 end
