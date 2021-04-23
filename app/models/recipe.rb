3ee542af716086017296bd6b13e6590dc6e6cf75
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-04-23 18:16:53 -0400

    Tweak order_by_likes scope method.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -12,7 +12,10 @@ class Recipe < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
   scope :order_by_recent, -> {order(created_at: :desc)}
-  scope :order_by_likes, -> {select('*').joins(:likes).group('recipes.id').order('COUNT(likes.likeable_id)')}
-  scope :most_used, -> {joins(:bakes).group('bakes.recipe_id').order('COUNT(bakes.recipe_id) DESC').limit(30)}
-  scope :most_discussed, -> {joins(:comments).group('comments.commentable_id').order('COUNT(comments.commentable_id) DESC').limit(30)}
+  scope :liked, -> {joins(:likes)}
+  scope :most_used, -> {joins(:bakes).group(:id).order('COUNT(bakes.recipe_id) DESC').limit(30)}
+  scope :most_discussed, -> {joins(:comments).group(:id).order('COUNT(comments.commentable_id) DESC').limit(30)}
+  # scope :order_by_likes, -> {select('*').joins(:likes).group('recipes.id, likes.id').order('COUNT(likes.id) DESC')}
+  scope :order_by_likes, -> {joins(:likes).group(:id).order('COUNT(likes.id) DESC')}
+
 end
