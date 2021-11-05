2e28d2789b5097f4be97d4d34787ba17fa74e0cd
Garrett-Bodley <garrett.bodley@gmail.com>
2021-11-05 12:28:45 -0400

    Utilize refactored scope methods in welcome controller.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 
   def home
-    @recipes_liked = Recipe.order_by_likes.limit(10)
+    @recipes_liked = Recipe.most_liked.limit(10)
     @recipes_used = Recipe.most_used.limit(10)
     @posts = Post.most_discussed.limit(10)
   end
