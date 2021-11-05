472d9bf4d7cb949465165512fd23b0285addd0d5
Garrett-Bodley <garrett.bodley@gmail.com>
2021-11-05 19:10:08 -0400

    Remove unnecessary line.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Faker::UniqueGenerator.clear
 
 def seed_dev_sandbox
   puts 'Seeding dev sandbox'
