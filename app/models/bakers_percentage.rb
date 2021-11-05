7718b60ab9c91161f0697d97fffe718933359fe8
Garrett-Bodley <garrett.bodley@gmail.com>
2021-11-05 17:45:41 -0400

    Fix code formatting.

--- app/models/bakers_percentage.rb
+++ app/models/bakers_percentage.rb
@@ -1,10 +1,8 @@
 class BakersPercentage < ApplicationRecord
   belongs_to :recipe
   belongs_to :ingredient
-  
 
   def percentify(weight)
-    self.percent * weight/100
+    percent * weight / 100
   end
-  
 end
