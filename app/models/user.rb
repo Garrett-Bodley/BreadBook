71fe4f141807cea17fbdc1f2c3fd7d67e0094995
Garrett-Bodley <garrett.bodley@gmail.com>
2021-11-13 17:06:36 -0500

    Delete associated records when deleting a user.
    Also organizes the code slightly better.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,33 +2,50 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :password, presence: true, allow_nil: true
   has_secure_password
-  has_many :bakes
-  has_many :comments
-  has_many :bookmarks
-  has_many :posts
+  has_many :bakes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :bookmarks, dependent: :destroy
+  has_many :posts, dependent: :destroy
   has_many :recipes
   has_many :ingredients
-  has_many :saved_ingredients, through: :bookmarks, source: :bookmarkable, source_type: 'Ingredient', class_name: 'Ingredient'
-  has_many :saved_recipes, through: :bookmarks, source: :bookmarkable, source_type: 'Recipe', class_name: 'Recipe'
-  has_many :saved_posts, through: :bookmarks, source: :bookmarkable, source_type: 'Post', class_name: 'Post'
+  has_many :saved_ingredients, {
+    through: :bookmarks,
+    source: :bookmarkable,
+    source_type: 'Ingredient',
+    class_name: 'Ingredient',
+    dependent: :destroy
+  }
+  has_many :saved_recipes, {
+    through: :bookmarks,
+    source: :bookmarkable,
+    source_type: 'Recipe',
+    class_name: 'Recipe',
+    dependent: :destroy
+  }
+  has_many :saved_posts, {
+    through: :bookmarks,
+    source: :bookmarkable,
+    source_type: 'Post',
+    class_name: 'Post',
+    dependent: :destroy
+  }
   has_many :likes
 
   scope :random, ->(limit = User.count) { order('RANDOM()').limit(limit) }
 
   def bookmarked?(object)
-    self.bookmarks.where(bookmarkable_id: object.id, bookmarkable_type: object.class.name).exists?
+    bookmarks.where(bookmarkable_id: object.id, bookmarkable_type: object.class.name).exists?
   end
 
   def find_bookmark_by_object(object)
-    self.bookmarks.find_by(bookmarkable_id: object.id, bookmarkable_type: object.class.name)
+    bookmarks.find_by(bookmarkable_id: object.id, bookmarkable_type: object.class.name)
   end
 
   def liked?(object)
-    self.likes.where(likeable_id: object.id, likeable_type: object.class.name).exists?
+    likes.where(likeable_id: object.id, likeable_type: object.class.name).exists?
   end
 
   def find_like_by_object(object)
-    self.likes.find_by(likeable_id: object.id, likeable_type: object.class.name)
+    likes.find_by(likeable_id: object.id, likeable_type: object.class.name)
   end
-
 end
